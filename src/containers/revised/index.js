1ee9975e8faacb991921a5ccff1efb868f99869a
Chris Fenos <chris.m.fenos@gmail.com>
2019-01-29 15:50:18 +0100

    get network and provider file removed & web3 removed -> issues resolved. Need to fix provider name issue next

--- src/containers/revised/index.js
+++ src/containers/revised/index.js
@@ -4,9 +4,9 @@ import Header from './components/Header';
 import './index.css'
 import Network from './components/bridge/Network';
 import BridgeForm from './components/bridge/BridgeForm';
-import provider from '../../scripts/provider';
-import getNetwork from '../../scripts/network';
 import { initializeNetwork } from '../store/actionCreator';
+
+const ethers = require('ethers');
 
 class BridgeContainer extends Component {
   state = {
@@ -15,12 +15,21 @@ class BridgeContainer extends Component {
   
   componentDidMount = async () => {
     try {
-      const selectedNetwork = await getNetwork();
-      const { providerObj, pubKey } = await provider();   
-      this.props.initializeNetwork({ selectedNetwork, providerObj, pubKey });
-      setTimeout(() => { 
-        this.displayBridge();
-      }, 1500);
+      window.addEventListener('load', async () => {
+        const { web3 } = window;
+        if (typeof web3 !== 'undefined') {
+          let pub = await window.ethereum.enable();  
+          let providerObj = await new ethers.providers.Web3Provider(window.web3.currentProvider)
+          console.log(providerObj);         
+          
+          this.props.initializeNetwork({ 'selectedNetwork': 'ropsten', providerObj, pubKey: pub[0] });
+          this.displayBridge();
+          //console.log({ providerObj });     
+        } else {
+          console.log('No web3? You should consider trying MetaMask!')
+          // add fallback
+        }
+      })
     } catch (err) {
       console.log(err);      
     }
