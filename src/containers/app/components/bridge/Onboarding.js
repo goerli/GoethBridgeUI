cb62c0db638e8c45503c112b96232883a52d078d
Chris Fenos <chris.m.fenos@gmail.com>
2019-02-06 16:54:52 +0100

    Closes #7 - closing issue #7 - intrusive metamask & user prompts to enable metamask

--- src/containers/app/components/bridge/Onboarding.js
+++ src/containers/app/components/bridge/Onboarding.js
@@ -9,7 +9,10 @@ class Onboarding extends React.Component {
             If you’d like to own some Ethereum but don’t have enough money to purchase any or 
             buy a mining rig, you may be interested in using an <b> Ethereum faucet. </b> The <b> G&ouml;rli Bridge </b> allows developers to exchange Ropsten, Kovan, or Rinkeby test net
             ether for G&ouml;eth. Please click the big blue button to open your MetaMask window.
-        </p>     
+        </p>
+        <p className="headerTextStyle">
+           <b> Please select either Ropsten, Rinkeby, or Kovan... :) </b>
+        </p>
       </div>
     );
   }
