d6aec034e1093396996e172d18ec05932e610fee
Chris Fenos <chris.m.fenos@gmail.com>
2019-02-06 16:50:25 +0100

    Onboarding section added to inform the user what the application is and that they must enable meta mask.

--- src/containers/app/components/bridge/Onboarding.js
+++ src/containers/app/components/bridge/Onboarding.js
@@ -1,7 +1,18 @@
-// import React from 'react';
+import React from 'react';
+import './Onboarding.css'
 
-// class Onboarding extends React.Component {
-//   render
-// }
+class Onboarding extends React.Component {
+  render() {
+    return (
+      <div className="onboardingContainerStyle"> 
+        <p className="headerTextStyle">            
+            If you’d like to own some Ethereum but don’t have enough money to purchase any or 
+            buy a mining rig, you may be interested in using an <b> Ethereum faucet. </b> The <b> G&ouml;rli Bridge </b> allows developers to exchange Ropsten, Kovan, or Rinkeby test net
+            ether for G&ouml;eth. Please click the big blue button to open your MetaMask window.
+        </p>     
+      </div>
+    );
+  }
+}
 
-// export default Onboarding;
+export default Onboarding;
